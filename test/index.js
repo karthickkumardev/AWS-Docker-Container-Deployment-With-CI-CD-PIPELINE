3cb951bbd6b50e49e3abaab276748b6cbcf4dbb6
karthi <karthicbe2405@gmail.com>
2022-02-11 12:05:14 +0530

    added failing test case

--- test/index.js
+++ test/index.js
@@ -6,7 +6,7 @@ describe('Some controller', () => {
   it('Get request to /test returns some text', async () => {
     const res = await request(app).get('/');
     const textResponse = res.body;
-    expect(res.status).to.equal(200);
+    expect(res.status).to.equal(400);
     // expect(textResponse.text).to.be.a('string');
     // expect(textResponse.text).to.equal('Simple Node App Working!');
   });
