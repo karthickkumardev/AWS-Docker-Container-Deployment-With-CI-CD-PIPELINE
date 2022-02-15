ad523b336559d7902ab72833d6a213aa323d691a
karthi <karthicbe2405@gmail.com>
2022-02-15 12:32:39 +0530

    stop fix 1

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #Stopping existing node servers
 echo "Stopping any existing node servers"
-pkill -9 node
-exit 0
+# pkill -9 node
+# exit 0
 # ps -ef | grep node | grep -v grep | awk '{print $2}' | xargs kill
 stat=`echo $?`
 echo $stat;
--- scripts/docker_application_stop.sh
+++ scripts/docker_application_stop.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 #Stopping existing node servers
-echo "Stopping any existing node servers"
-pkill node
-
-stat=`echo $?`
-if [ $stat -eq 1  ]; then
-  exit 0
-fi
+echo "Stopping any existing Docker Container"
+docker ps | grep "carserver" | grep -v grep | awk '{print $1}' | xargs docker stop
