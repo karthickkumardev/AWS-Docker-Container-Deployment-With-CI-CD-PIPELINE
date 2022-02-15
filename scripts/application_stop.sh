b48f1f898e248cc9ab6262a510f76b4bf94299d2
karthi <karthicbe2405@gmail.com>
2022-02-15 12:18:37 +0530

    stop script fix v1

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #Stopping existing node servers
 echo "Stopping any existing node servers"
-# pkill -9 node
-ps -ef | grep node | grep -v grep | awk '{print $2}' | xargs kill
+pkill -9 node
+exit 0
+# ps -ef | grep node | grep -v grep | awk '{print $2}' | xargs kill
 stat=`echo $?`
 echo $stat;
